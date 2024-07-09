1a9bc5315ff025fcb9aa4da2ff3167ad3dbe17af
Pavlov Denis <pda33@icloud.com>
2024-07-09 19:12:31 +0300

    canceled last changes

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -10,21 +10,15 @@ public class BaseSchema<T> {
     protected List<Predicate<T>> conditions = new ArrayList<>();
     @Setter
     protected boolean requiredStatus;
-    protected boolean shapeStatus;
     public void addCondition(Predicate<T> predicate) {
         conditions.add(predicate);
     }
 
     public boolean isValid(T obj) {
-        boolean result;
         if (obj == null) {
             return !requiredStatus;
         } else {
-            result = conditions.stream().allMatch(condition -> condition.test(obj));
+            return conditions.stream().allMatch(condition -> condition.test(obj));
         }
-        if (!shapeStatus) {
-            conditions.clear();
-        }
-        return result;
     }
 }
